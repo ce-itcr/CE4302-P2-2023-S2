dd45219de88da9c38c2b9611e7b1434bc244b3e8
delias2798 <32471383+delias2798@users.noreply.github.com>
2023-11-21 17:07:21 -0600

    [UPDATE] Image default partition

--- Tools/ImageToBinary.py
+++ Tools/ImageToBinary.py
@@ -33,23 +33,51 @@ example_image_path = os.path.join(script_directory, "..", "Images", "VectorImage
 binary_pixels = image_to_binary(example_image_path)
 # binary_pixels[:10]  # Displaying the first 10 binary pixels for demonstration purposes
 
-text_file_path = os.path.join(script_directory, "..", "ImagesTxt", "BN_Image_Test.txt")
 
-# text_file_path = script_directory + '../ImagesTxt/BN_Image_Test.txt'
+# text_file_path = os.path.join(script_directory, "..", "ImagesTxt", "BN_Image_Test101888.txt")
 
-with open(text_file_path, 'w') as file:
-    for binary_pixel in binary_pixels:
-        file.write(binary_pixel + '\n')
+# # text_file_path = script_directory + '../ImagesTxt/BN_Image_Test.txt'
+# breakPoint = 101888
+# with open(text_file_path, 'w') as file:
+#     count = 0
+#     for binary_pixel in binary_pixels:
+#         if count <=  breakPoint:
+#             file.write(binary_pixel + '\n')
+#             count+=1
+#         else:
+#             break
 
-lines_to_prepend = [
-    "// instance=/testbench/DUT/tess/dmem/RAM",
-    "// format=bin addressradix=h dataradix=b version=1.0 wordsperline=1 noaddress"
-]
+script_directory = os.path.dirname(os.path.abspath(__file__))
+pixels_per_file = 10240
+file_count = 0
+count = 0
 
-with open(text_file_path, 'r') as file:
-    current_contents = file.readlines()
+file_name = f"BN_Image_Test{file_count}.txt"
+text_file_path = os.path.join(script_directory, "..", "ImagesTxt", file_name)
+file = open(text_file_path, 'w')
 
-with open(text_file_path, 'w') as file:
-    for line in lines_to_prepend:
-        file.write(line + '\n')
-    file.writelines(current_contents)
+for binary_pixel in binary_pixels:
+    if count > 0 and count % pixels_per_file == 0:
+        file.close()  # Close the current file
+        file_count += 1
+        file_name = f"BN_Image_Test{file_count}.txt"
+        text_file_path = os.path.join(script_directory, "..", "ImagesTxt", file_name)
+        file = open(text_file_path, 'w')
+
+    file.write(binary_pixel + '\n')
+    count += 1
+
+file.close()
+
+# lines_to_prepend = [
+#     "// instance=/testbench/DUT/tess/dmem/RAM",
+#     "// format=bin addressradix=h dataradix=b version=1.0 wordsperline=1 noaddress"
+# ]
+
+# with open(text_file_path, 'r') as file:
+#     current_contents = file.readlines()
+
+# with open(text_file_path, 'w') as file:
+#     for line in lines_to_prepend:
+#         file.write(line + '\n')
+#     file.writelines(current_contents)
